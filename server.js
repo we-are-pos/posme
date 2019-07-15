c38c20dafdd3061dac6d38fc04926c011562418c
The_Costa <bryan.costa@roadrunner.com>
2019-07-14 17:22:11 -0700

    server file creation

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const { join } = require('path')
+const passport = require('passport')
+const { Strategy } = require('passport-local')
+const { Strategy: JWTStrategy, ExtractJwt } = require('passport-jwt')
+const { User } = require('./models')
+const app = express()
+
+app.use(express.static(join(__dirname, 'client', 'build')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(require('express-session')({
+    secret: 'hotdog',
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.use(new Strategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+passport.use(new JWTStrategy({
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: 'hotdog'
+}, (jwtPayload, cb) => User.findById(jwtPayload.id)
+    .then(user => cb(null, user))
+    .catch(err => cb(err, null))
+))
+
+require('./routes')(app)
+
+require('mongoose').connect('mongodb://localhost/posme_db', { useNewUrlParser: true, useFindAndModify: true, useCreateIndex: true })
+    .then(_ => app.listen(process.env.PORT || 3001))
+    .catch(e => console.log(e))
