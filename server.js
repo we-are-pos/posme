9bb22225252d4fc6b2b5ec702a01e0445a34cc69
The_Costa <bryan.costa@roadrunner.com>
2019-07-14 18:49:23 -0700

    database changes

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ passport.use(new JWTStrategy({
 
 require('./routes')(app)
 
-require('mongoose').connect('mongodb://localhost/posme_db', { useNewUrlParser: true, useFindAndModify: true, useCreateIndex: true })
+require('mongoose').connect('mongodb://localhost:27017/posme_db', { useNewUrlParser: true, useFindAndModify: true, useCreateIndex: true })
     .then(_ => app.listen(process.env.PORT || 3001))
     .catch(e => console.log(e))
