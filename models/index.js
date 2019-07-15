84536243b6fdfc255ac8ffb8798f6bbfe20e3de3
The_Costa <bryan.costa@roadrunner.com>
2019-07-14 19:24:16 -0700

    Added Model files for db

--- models/index.js
+++ models/index.js
@@ -2,4 +2,8 @@ const { Schema, model } = require('mongoose')
 
 module.exports = {
     User: require('./User.js')(Schema, model)
+    Company: require('./Company.js')(Schema, model)
+    Category: require('./Category.js')(Schema, model)
+    Item: require('./Item.js')(Schema, model)
+    Order: require('./Order.js')(Schema, model)
 }
