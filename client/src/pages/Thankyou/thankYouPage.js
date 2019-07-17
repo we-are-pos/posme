39c7d61d9d1f40a075b59a6aaf0404859300a134
Kumiko Nomura <kumiko.nomura@laverne.edu>
2019-07-17 15:50:49 -0700

    added components, just need to fix styling of buttons

--- client/src/pages/Thankyou/thankYouPage.js
+++ client/src/pages/Thankyou/thankYouPage.js
@@ -2,19 +2,35 @@ import React from 'react'
 import KingCactus from './images/giphy.png'
 import './thankYouPage.css'
 
-
 const ThankYouPage = _ => {
   return (
     <>
       <img alt='cactus' className='kingcactus' src={KingCactus} />
       <h1 className='thankYou'>Thank You</h1>
       <h5 className='receipt'>Receipt?</h5>
-      <div>
+      <div className='printRow'>
         <button
           className='print'
-          type='button'
-        >
+          type='button' >
           Print
+        </button>
+        <div className='divider' />
+        <button
+          className='text'
+          type='button' >
+          Text
+        </button>
+        <div className='divider' />
+        <button
+          className='email'
+          type='button' >
+          Email
+        </button>
+        <div className='divider' />
+        <button
+          className='cancel'
+          type='button' >
+          Cancel
         </button>
       </div>
     </>
