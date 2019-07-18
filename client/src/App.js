5619f8e5dbefd71177aef062a78de09b101ec257
nina <ninaw1@uci.edu>
2019-07-17 18:36:49 -0700

    calculator and card

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react'
+import Calculator from './components/Calculator'
+import Card from './components/Card'
+import MediaCard from './components/Card';
 
 class App extends Component {
   render () {
     return (
+      <>
       <h1>We Are POSME</h1>
+      <Calculator />
+      <Card />
+      </>
     )
   }
 }
