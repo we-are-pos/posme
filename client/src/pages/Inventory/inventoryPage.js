539e17dcb76184865a30f28540ec0f7a9dfa7e1b
Winsam Roderos <Winsamhehe@Winsams-MacBook-Pro.local>
2019-07-18 23:53:40 -0700

    navbar

--- client/src/pages/Inventory/inventoryPage.js
+++ client/src/pages/Inventory/inventoryPage.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import NavBar from '../../components/Navbar'
+
 
 class Inventory extends React.Component {
   render () {
     return (
+      <div>
+      <NavBar />
       <h1>Inventory Page</h1>
+      </div>
     )
   }
 }
 
 export default Inventory
+
