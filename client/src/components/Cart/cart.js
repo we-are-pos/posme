534f90c81450eb99b38eb1cf7e1bb594b3af293e
Winsam Roderos <Winsamhehe@dhcp-v067-219.mobile.uci.edu>
2019-07-20 12:30:08 -0700

    update1

--- client/src/components/Cart/cart.js
+++ client/src/components/Cart/cart.js
@@ -26,7 +26,7 @@ const useStyles = makeStyles(theme => ({
     flexShrink: 0
   },
   drawerPaper: {
-    width: drawerWidth
+    width: 300,
   },
   toolbar: theme.mixins.toolbar,
   content: {
--- client/src/components/Navbar/navbar.js
+++ client/src/components/Navbar/navbar.js
@@ -5,7 +5,7 @@ import IconButton from '@material-ui/core/IconButton';
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
-// import Menu from '@material-ui/icons/MoreHoriz';
+import Menu from '@material-ui/icons/MoreHoriz';
 
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
@@ -34,9 +34,15 @@ title: {
   textAlign: 'center',
 },
 navBar: {
-  width: 994,
   backgroundColor: '#4E5152',
-}
+},
+list: {
+  width:350,
+  height: 500,
+},
+fullList: {
+  width: 'auto',
+},
   
 }));
 
