b0f62b15418a8586185131229a15f925a05152a3
Winsam Roderos <Winsamhehe@dhcp-v090-203.mobile.uci.edu>
2019-07-24 19:29:43 -0700

    initial commit

--- client/src/components/Calculator/calculator.js
+++ client/src/components/Calculator/calculator.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import Button from '@material-ui/core/Button'
+import Grid from "@material-ui/core/Grid"
+
 
 const defaultCalc = {
   total: null,
@@ -28,33 +30,72 @@ const Calculator = _ => {
   }
 
   return (
-    <div>
-      <h1>{calcState.total}</h1>
-      <div>
-        <Button variant="contained" color="primary" id='1' onClick={getDigit}>1</Button>
-        <Button variant="contained" color="primary" id='2' onClick={getDigit}>2</Button>
-        <Button variant="contained" color="primary" id='3' onClick={getDigit}>3</Button>
-        <br />
+    <div style={{ height: '700px'}} >
+      {/* <h1>{calcState.total}</h1> */}
 
-        <Button variant="contained" color="primary" id='4' onClick={getDigit}>4</Button>
-        <Button variant="contained" color="primary" id='5' onClick={getDigit}>5</Button>
-        <Button variant="contained" color="primary" id='6' onClick={getDigit}>6</Button>
-        <br />
+      <Grid container spacing={4}>
+      <Grid item xs={12}>
+        <h1>{calcState.total}</h1>
+        </Grid>
+
+        <Grid item xs={3}>
+        <Button variant="contained" color="default" id='10' onClick={(e) => getDigit(e, true)}>$10</Button>
+        </Grid>
+        <Grid item xs={3}>
 
         <Button variant="contained" color="primary" id='7' onClick={getDigit}>7</Button>
-        <Button variant="contained" color="primary" id='8' onClick={getDigit}>8</Button>
-        <Button variant="contained" color="primary" id='9' onClick={getDigit}>9</Button>
-        <br />
+        </Grid>
+        <Grid item xs={3}>
 
-        <Button variant="contained" color="primary" id='0' onClick={getDigit}>0</Button>
-      </div>
-      <div>
-        <Button variant="contained" color="default" id='10' onClick={(e) => getDigit(e, true)}>$10</Button>
+        <Button variant="contained" color="primary" id='8' onClick={getDigit}>8</Button>
+        </Grid>
+        <Grid item xs={3}>
+
+        <Button variant="contained" color="primary" id='9' onClick={getDigit}>9</Button>
+        </Grid>
+
+        <Grid item xs={3}>
         <Button variant="contained" color="default" id='20' onClick={(e) => getDigit(e, true)}>$20</Button>
+        </Grid>
+
+        <Grid item xs={3}>
+        <Button variant="contained" color="primary" id='4' onClick={getDigit}>4</Button>
+        </Grid>
+
+        <Grid item xs={3}>
+        <Button variant="contained" color="primary" id='5' onClick={getDigit}>5</Button>
+        </Grid>
+
+        <Grid item xs={3}>
+        <Button variant="contained" color="primary" id='6' onClick={getDigit}>6</Button>
+        </Grid>
+
+        <Grid item xs={3}>
+
         <Button variant="contained" color="default" id='50' onClick={(e) => getDigit(e, true)}>$50</Button>
+        </Grid>
+        <Grid item xs={3}>
+
+        <Button variant="contained" color="primary" id='1' onClick={getDigit}>1</Button>
+        </Grid>
+        <Grid item xs={3}>
+
+        <Button variant="contained" color="primary" id='2' onClick={getDigit}>2</Button>
+        </Grid>
+        <Grid item xs={3}>
+        <Button variant="contained" color="primary" id='3' onClick={getDigit}>3</Button>
+        </Grid>
+
+        <Grid item xs={4}>
         <Button variant="contained" color="primary" onClick={clear}>ENTER</Button>
+        </Grid>
+        <Grid item xs={4}>
+        <Button variant="contained" color="primary" id='0' onClick={getDigit}>0</Button>
+        </Grid>
+        <Grid item xs={4}>
         <Button variant="contained" color="secondary" onClick={clear}>DELETE</Button>
-      </div>
+      </Grid>
+      </Grid>
     </div>
   )
 }
