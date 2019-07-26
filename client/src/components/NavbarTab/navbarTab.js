eea57b5ac9b8d18866ea2a8d24013fbd5a3e68d5
Winsam Roderos <Winsamhehe@Winsams-MacBook-Pro.local>
2019-07-25 23:51:25 -0700

    update for presentation

--- client/src/components/NavbarTab/navbarTab.js
+++ client/src/components/NavbarTab/navbarTab.js
@@ -1,51 +1,155 @@
-import React, { useState, useEffect } from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import Paper from '@material-ui/core/Paper'
-import Tabs from '@material-ui/core/Tabs'
-import Tab from '@material-ui/core/Tab'
-import ItemCardList from '../Card'
+// import React, { useState, useEffect } from 'react'
+// import { makeStyles } from '@material-ui/core/styles'
+// import Paper from '@material-ui/core/Paper'
+// import Tabs from '@material-ui/core/Tabs'
+// import Tab from '@material-ui/core/Tab'
+// import ItemCardList from '../Card'
 
-const useStyles = makeStyles({
-    root: {
-        flexGrow: 1,
-    },
-})
-
-
-export default function CenteredTabs(props) {
-    const classes = useStyles()
-    const [value, setValue] = useState(0)
-    const [tabId, setTabId] = useState(0)
+// const useStyles = makeStyles({
+//     root: {
+//         flexGrow: 1,
+//     },
+// })
 
 
-    useEffect(() => {
-        props.handleGetTabs({})
-    }, [])
+// export default function CenteredTabs(props) {
+//     const classes = useStyles()
+//     const [value, setValue] = useState(0)
+//     const [tabId, setTabId] = useState(0)
 
-    function handleChange(event, newValue) {
-        setTabId(event.currentTarget.id)
-        setValue(newValue)
-    }
 
-    return (
-        <Paper className={classes.root}>
-            <Tabs
-                value={value}
-                onChange={handleChange}
-                onClick={props.handleGetTabs}
-                indicatorColor="primary"
-                textColor="primary"
-            >
-                {props.tabs.map((tabs, i) => {
-                    return <Tab id={tabs._id} label={tabs.name} />
-                })}
-            </Tabs>
-            {
-                <ItemCardList
-                    items={props.items.filter(x => x.tab === tabId)}
-                    tabs={props.tabs}
-                    handleGetTabs={props.handleGetTabs}
-                />}
-        </Paper>
-    );
+//     useEffect(() => {
+//         props.handleGetTabs({})
+//     }, [])
+
+//     function handleChange(event, newValue) {
+//         setTabId(event.currentTarget.id)
+//         setValue(newValue)
+//     }
+
+//     return (
+//         <Paper className={classes.root}>
+//             <Tabs
+//                 value={value}
+//                 onChange={handleChange}
+//                 onClick={props.handleGetTabs}
+//                 indicatorColor="primary"
+//                 textColor="primary"
+//             >
+//                 {props.tabs.map((tabs, i) => {
+//                     return <Tab id={tabs._id} label={tabs.name} />
+//                 })}
+//             </Tabs>
+//             {
+//                 <ItemCardList
+//                     items={props.items.filter(x => x.tab === tabId)}
+//                     tabs={props.tabs}
+//                     handleGetTabs={props.handleGetTabs}
+//                 />}
+//         </Paper>
+//     );
+// }
+
+
+
+import React from 'react';
+import PropTypes from 'prop-types';
+// import SwipeableViews from 'react-swipeable-views';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      <Box p={3}>{children}</Box>
+    </Typography>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `full-width-tab-${index}`,
+    'aria-controls': `full-width-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    backgroundColor: theme.palette.background.paper,
+    width: 999,
+  },
+}));
+
+export default function FullWidthTabs() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+
+  function handleChange(event, newValue) {
+    setValue(newValue);
+  }
+
+  function handleChangeIndex(index) {
+    setValue(index);
+  }
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="default">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="fullWidth"
+          aria-label="full width tabs example"
+        >
+          <Tab label="Mini Succulents" {...a11yProps(0)} />
+          <Tab label="Arrangements" {...a11yProps(1)} />
+          <Tab label="Hens & Chicks" {...a11yProps(2)} />
+          <Tab label="Wild Flowers" {...a11yProps(2)} />
+        </Tabs>
+      </AppBar>
+      {/* <SwipeableViews */}
+        {/* axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={value}
+        onChangeIndex={handleChangeIndex}
+      > */}
+        <TabPanel value={value} index={0} dir={theme.direction}>
+        place " mini Succulents image here"
+
+        </TabPanel>
+        <TabPanel value={value} index={1} dir={theme.direction}>
+        place " Arrangements image here"
+
+        </TabPanel>
+        <TabPanel value={value} index={2} dir={theme.direction}>
+        place " Hens and Chicks  image here"
+
+        </TabPanel>
+        <TabPanel value={value} index={3} dir={theme.direction}>
+        place "  Wild Flowers image here"
+        </TabPanel>
+      {/* </SwipeableViews> */}
+    </div>
+  );
 }
--- client/src/pages/Home/homePage.js
+++ client/src/pages/Home/homePage.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
 import NavBar from '../../components/Navbar'
 import Cart from '../../components/Cart'
-import CenteredTabs from '../../components/NavbarTab'
-import axios from 'axios'
+import NavbarTab from '../../components/NavbarTab'
+// import CenteredTabs from '../../components/NavbarTab'
+// import axios from 'axios'
+
 
 class Home extends Component {
   state = {
@@ -10,37 +12,39 @@ class Home extends Component {
     tabs: [],
     items: []
   }
-  handleGetTabs = _ => {
-    axios.get(`/tab/${localStorage.getItem('company')}`)
-      .then(({ data }) => {
-        const tabIdArr = []
-        data.map(tab => {
-          return tabIdArr.push(tab._id)
-        })
-        this.setState({ ...this.state, tabs: data, tabId: tabIdArr })
-        this.state.tabId.map(id => {
-          return axios.get(`/item`, {
-            tabs: id
-          })
-            .then(({ data }) => {
-              this.setState({ ...this.state, items: data })
-            })
-            .catch(e => console.log(e))
-        })
-      })
-      .catch(e => console.log(e))
-  }
+  // handleGetTabs = _ => {
+  //   axios.get(`/tab/${localStorage.getItem('company')}`)
+  //     .then(({ data }) => {
+  //       const tabIdArr = []
+  //       data.map(tab => {
+  //         return tabIdArr.push(tab._id)
+  //       })
+  //       this.setState({ ...this.state, tabs: data, tabId: tabIdArr })
+  //       this.state.tabId.map(id => {
+  //         return axios.get(`/item`, {
+  //           tabs: id
+  //         })
+  //           .then(({ data }) => {
+  //             this.setState({ ...this.state, items: data })
+  //           })
+  //           .catch(e => console.log(e))
+  //       })
+  //     })
+  //     .catch(e => console.log(e))
+  // }
 
   render () {
     return (
       <div>
       <NavBar />
+      <NavbarTab />
       <Cart />
-      <CenteredTabs 
+
+      {/* <CenteredTabs 
           items={this.state.items}
           tabs={this.state.tabs}
           handleGetTabs={this.handleGetTabs}
-      />
+      /> */}
       </div>
     )
   }
